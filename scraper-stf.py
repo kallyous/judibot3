2a3070a6dd54ca651261caa883a50a211e630716
Lucas Carvalho Flores <lcf@ic.ufal.br>
2020-11-24 08:14:16 -0300

    Adiciona parametros para escolher datas de publicação, tamanho da paginação, etc.

--- scraper-stf.py
+++ scraper-stf.py
@@ -16,20 +16,19 @@ import time
 # URL base da página de busca
 SITE_HOME = 'https://jurisprudencia.stf.jus.br/'
 
-# Resultados por página.
-BATCH_SIZE = 5
-
 # Tolerar no máximo essa quantidade de erros de conexão consecutivos.
 CONNECTION_ERROR_TOLERANCE = 10
 
 # Usar query = BASE_QUERY.replace('{1}', page_number) para paginação.
-BASE_QUERY = 'pages/search?base={base}&sinonimo=true&plural=true&page={page}&pageSize='+str(BATCH_SIZE)+'&queryString={termo}&sort=_score&sortBy=desc'
+# BASE_QUERY = 'pages/search?base={base}&sinonimo=true&plural=true&page={page}&pageSize='+str(BATCH_SIZE)+'&queryString={termo}&sort=_score&sortBy=desc'
+BASE_QUERY = 'pages/search?base={base}&pesquisa_inteiro_teor=false&sinonimo=true&plural=true&radicais=false&buscaExata=true&publicacao_data={data_ini}-{data_fim}&page={page}&pageSize={res_pg}&queryString={termo}&sort=_score&sortBy=desc'
 
 # Acha no HTML da página o número de documentos retornados pela busca.
 DOC_COUNT_RE = '(\d+\.*\d*) resultado\(s\) para:'
 
 # Acha na URL do documento o ID da ementa, para salvar-mos sem duplicatas.
-DOC_ID_RE = '.*sjur(\d+).*'
+# DOC_ID_RE = '.*sjur(\d+).*'
+DOC_ID_RE = r'search/(\w+-*\w*)/'
 
 # User-Agent aleatório
 USER_AGENT = UserAgent().random
@@ -40,7 +39,7 @@ SPLASH_SCRIPT = "    headers = { ['User-Agent'] = '" + USER_AGENT + """',}
     splash.private_mode_enabled = false
     splash.images_enabled = false
     assert(splash:go(args.url))
-    assert(splash:wait(1))
+    assert(splash:wait(5))
     return splash:html()
 """
 
@@ -51,6 +50,9 @@ client = None          # Global de conexão com BD.
 db = None              # Global pra acessar a conexão aberta.
 connection_erros = 0   # Global para rastrear erros de conexão.
 timeout = 1            # Global para marcar espera entre acesso a páginas.
+data_ini = ''          # Global para data mais antiga de publicação dos docs.
+data_fim = None        # Global para data mais recente de publicação dos docs.
+res_pg = 50            # Global para quantidade de resultados por página.
 
 
 def flagConnectionError(sleep_time=15):
@@ -72,7 +74,7 @@ def scrapAcordaoPage(url):
     doc_id = re.search(DOC_ID_RE, url).group(1)
     
     # Prepara dicionário para segurar os dados coletados.
-    document = {'_id': int(doc_id), 'url': url}
+    document = {'_id': doc_id, 'url': url}
     
     # Carrega página do acórdão usando Splash.
     resp = requests.post(
@@ -86,7 +88,7 @@ def scrapAcordaoPage(url):
     main_div = tree.xpath("//div[contains(@class, 'cp-content display-in-print ng-star-inserted')]")[0]
     
     # Se essa div não estiver presente, tivemos erro de conexão.
-    if not main_div:
+    if len(main_div) < 1:
         return None  # Como não temos loop aqui dentro, trataremos o erro fora.
     
     # Se tudo ocorreu bem, essa div tem os dados principais do acórdão.
@@ -123,7 +125,10 @@ def buildPaginationURL(termo, base, indice, extras=None):
         return urljoin(SITE_HOME, BASE_QUERY
                        .replace('{base}', base)
                        .replace('{page}', indice_str)
-                       .replace('{termo}', termo_enc))
+                       .replace('{termo}', termo_enc)
+                       .replace('{data_ini}', data_ini )
+                       .replace('{data_fim}', data_fim)
+                       .replace('{res_pg}', str(res_pg)) )
     else:
         print('Pesquisa por', base, 'não implementado.')
         exit(1)
@@ -183,16 +188,35 @@ def updateDatabase(documents, base):
 @click.command()
 @click.option('--termo', default='associação ilícita', help='Termo de busca')
 @click.option('--espera', default=60, help='Tempo em segundos de espera entre cada documento acessado. Default 60s.')
-@click.option('--maxpg', default=0, help='Quantidade máxima de páginas de busca a serem analisadas.')
+@click.option('--max-pg', default=0, help='Quantidade máxima de páginas de busca a serem analisadas.')
 @click.option('--base', default='acordaos', help='Base de dados do STF onde buscar os documentos: '
               ' acordaos, decisoes-monocr, decisoes-presid, informativos, sumulas, sumulas-vinc, todas')
-def scrap(termo, base, espera, maxpg):
+@click.option('--data-inicial', default='', help='Usar dd-mm-aaaa ou dd/mm/aaaa. Data mais antiga limite de até quando retornar documentos antigos. Default para desde o começo.')
+@click.option('--data-final', default=None, help='Usar dd-mm-aaaa ou dd/mm/aaaa. Data recente limite de até quando retornar documentos recentes. Default para data de execução do bot.')
+@click.option('--res-por-pag', default=50, help='Quantidade de resultados por página de paginação/resultados da busca. Default 50, recomendado 25, 50 ou 100.')
+def scrap(termo, base, espera, max_pg, data_inicial, data_final, res_por_pag):
     global db
     global client
     global timeout
+    global data_ini
+    global data_fim
+    global res_pg
     
     # Ajusta timeout
     timeout = espera
+    
+    # Data inicial é vazia para pegar desde o começo ou recebida pelo usuário.
+    data_ini = data_inicial.replace('-', '').replace('/', '')
+    
+    # Data final é fornecida pelo usuário ou definida para o dia atual.
+    if data_final:
+        data_fim = data_final.replace('-', '').replace('/', '')
+    else:
+        lt = time.localtime(time.time())
+        data_fim = f'{lt[2]}{lt[1]}{lt[0]}'
+    
+    # Define quantidade de resultados por página
+    res_pg = res_por_pag
     
     # Abre conexão com banco de dados.
     client = MongoClient(DB_URL)
@@ -221,13 +245,13 @@ def scrap(termo, base, espera, maxpg):
         #3 Atualiza last_page se estiver na primeira iteração.
         if page_num == 1:
             
-            # Quando maxpg é nãp-positivo, processar até a última página.
-            if maxpg < 1:
+            # Quando max_pg é nãp-positivo, processar até a última página.
+            if max_pg < 1:
                 match = re.search(DOC_COUNT_RE, remove_tags(resp.text))
                 if match:
                     doc_count = match.group(1).replace('.','')
                     doc_count = int(doc_count)
-                    last_page = math.ceil(doc_count/BATCH_SIZE)
+                    last_page = math.ceil(doc_count/res_pg)
                     # Reseta contagem de erros.
                     clearConnectionErrorFlags()
                     
@@ -237,7 +261,7 @@ def scrap(termo, base, espera, maxpg):
                     continue              # Reinicia iteração atual.
             
             # Quando positivo, essa será a útima página a processar.
-            else: last_page = maxpg
+            else: last_page = max_pg
         
         print('PAGINAÇÂO - final em', last_page)
         print()
